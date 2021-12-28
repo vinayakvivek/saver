f516e1d4a60008e0a66cf7b86ede098e59deb1b4
Vinayak <vinayakdeepu@gmail.com>
2021-12-28 23:20:54 +0530

    Add AccountTile class

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:saver/account.dart';
 import 'package:saver/boxes.dart';
 import 'package:saver/screens/add_account_screen.dart';
+import 'package:saver/screens/splash_screen.dart';
+
+class AccountTile extends HookConsumerWidget {
+  const AccountTile({Key? key, required this.account}) : super(key: key);
+  final Account account;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final localAuth = ref.read(localAuthProvider);
+    final showPassword = useState(false);
+    return ListTile(
+      title: Text(account.name),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(account.username),
+          Text(showPassword.value ? account.password : "******"),
+        ],
+      ),
+      leading: const FlutterLogo(size: 56.0),
+      onTap: () async {
+        if (showPassword.value) {
+          showPassword.value = false;
+          return;
+        }
+        final auth =
+            await localAuth.authenticate(localizedReason: "specific auth");
+        if (auth) {
+          showPassword.value = true;
+        }
+      },
+    );
+  }
+}
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -17,9 +53,7 @@ class HomeScreen extends StatelessWidget {
           return ListView.builder(
             itemBuilder: (context, index) {
               final account = accounts[index];
-              return ListTile(
-                title: Text(account.name),
-              );
+              return AccountTile(account: account);
             },
             itemCount: accounts.length,
           );
